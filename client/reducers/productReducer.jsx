202bb408e3f4a8a728ad22c86d79e259ea617db0
Yates Walker <yateswalker@gmail.com>
2018-01-09 09:44:48 -0600

    comments out part of reducer

--- client/reducers/productReducer.jsx
+++ client/reducers/productReducer.jsx
@@ -2,19 +2,19 @@ import axios from 'axios';
 
 // action types
 const GET_ALL_PRODUCTS = 'GET_ALL_PRODUCTS';
-const PUT_NEW_PRODUCT = 'PUT_NEW_PRODUCT';
-const UPDATE_PRODUCT = 'UPDATE_PRODUCT';
-const DELETE_PRODUCT = 'DELETE_PRODUCT';
-const ADD_PRODUCT_TO_CART = 'ADD_PRODUCT_TO_CART';
+// const PUT_NEW_PRODUCT = 'PUT_NEW_PRODUCT';
+// const UPDATE_PRODUCT = 'UPDATE_PRODUCT';
+// const DELETE_PRODUCT = 'DELETE_PRODUCT';
+// const ADD_PRODUCT_TO_CART = 'ADD_PRODUCT_TO_CART';
 
 // action creators
     // customer actions
-export const addProductToCart = (productObj) => {
-    const newAction = {};
-    newAction.productObj = productObj;
-    newAction.type = ADD_PRODUCT_TO_CART;
-    return newAction;
-}
+// export const addProductToCart = (productObj) => {
+//     const newAction = {};
+//     newAction.productObj = productObj;
+//     newAction.type = ADD_PRODUCT_TO_CART;
+//     return newAction;
+// }
     // customer/admin actions
 export const getAllProducts = (productObjList) => {
     const newAction = {};
@@ -23,26 +23,26 @@ export const getAllProducts = (productObjList) => {
     return newAction;
 }
     // admin actions
-export const makeNewProduct = (productObj) => {
-    const newAction = {};
-    newAction.productObj = productObj;
-    newAction.type = PUT_NEW_PRODUCT;
-    return newAction;
-}
+// export const makeNewProduct = (productObj) => {
+//     const newAction = {};
+//     newAction.productObj = productObj;
+//     newAction.type = PUT_NEW_PRODUCT;
+//     return newAction;
+// }
 
-export const updateProduct = (productObj) => {
-    const newAction = {};
-    newAction.productObj = productObj;
-    newAction.type = UPDATE_PRODUCT;
-    return newAction;
-}
+// export const updateProduct = (productObj) => {
+//     const newAction = {};
+//     newAction.productObj = productObj;
+//     newAction.type = UPDATE_PRODUCT;
+//     return newAction;
+// }
 
-export const deleteProduct = productObj => {
-    const newAction = {};
-    newAction.type = DELETE_PRODUCT;
-    newAction.productObj = productObj;
-    return newAction;
-}
+// export const deleteProduct = productObj => {
+//     const newAction = {};
+//     newAction.type = DELETE_PRODUCT;
+//     newAction.productObj = productObj;
+//     return newAction;
+// }
 
 // thunk creators
 export function fetchAllProducts() {
