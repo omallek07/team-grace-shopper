7ce18a5769e80d5dae7cd7dfe1d7ca2d21ad0227
Yates Walker <yateswalker@gmail.com>
2018-01-09 17:02:33 -0600

    created adminDashboard.jsx

---
+++ client/components/AdminDashboard.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import history from '../history';
+
+function AdminDashboard () {
+
+    return (
+        <div>
+          <h1> Admin dashboard placeholder </h1>
+        </div>
+    )
+}
+
+export default AdminDashboard;
