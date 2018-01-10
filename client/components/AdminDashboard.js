84fe044ce28941cf877de8b0c720b56b864d7551
Kevin <omallek@gmail.com>
2018-01-10 14:10:57 -0600

    admin all orders component made

--- client/components/AdminDashboard.js
+++ client/components/AdminDashboard.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import history from '../history';
+import AdminOrdersList from './AdminOrdersList';
 
 function AdminDashboard () {
 
     return (
         <div>
           <h1> Admin dashboard placeholder </h1>
+          {
+            /* potential display for all orders within admin dashboard */
+          }
+          <AdminOrdersList />
         </div>
     )
 }
