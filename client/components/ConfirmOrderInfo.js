5ab78a9bd685bd0b90236df095771784b9ae0d81
Kevin <omallek@gmail.com>
2018-01-10 13:55:08 -0600

    create info confirm for order component

---
+++ client/components/ConfirmOrderInfo.js
@@ -0,0 +1,9 @@
+import React from 'react-redux';
+
+const ConfirmOrderInfo = () => {
+  return (
+    <h1>This will confirm shipping address info and book order</h1>
+  )
+}
+
+export default ConfirmOrderInfo;
