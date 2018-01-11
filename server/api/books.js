884872acef0afd301031b20a907bfe37db9b794a
mikekanter6@gmail.com <mikekanter@Mikes-MacBook-Pro.local>
2018-01-11 10:23:22 -0600

    all books route and nodemon on package.json

--- server/api/books.js
+++ server/api/books.js
@@ -3,35 +3,11 @@ const {Book} = require('../db')
 module.exports = router
 
 router.get('/', async (req, res, next) => {
-  let booksPromise = Book.findAll()
+  let booksPromise = Book.findAll({include: {
+    all: true}
+  })
 
   let books = await booksPromise
 
-  let genrePromiseArray = Promise.all(books.map(book => {
-    return book.getGenres()
-    .then(genres => genres[0] && genres.map(genre => (
-      {id: genre.dataValues.id, name: genre.dataValues.name}
-    )))
-  })).catch(console.error)
-
-  let authorPromiseArray = Promise.all(books.map(book => {
-    return book.getAuthors()
-    .then(authors => authors[0] && authors.map(author => (
-      {id: author.dataValues.id, firstName: author.dataValues.firstName, lastName: author.dataValues.lastName}
-    )))
-  })).catch(console.error)
-
-  const genreArray = await genrePromiseArray
-  const authorArray = await authorPromiseArray
-
-  let bookArray = books.map((book, i) => {
-    let genres = genreArray[i]
-    let authors = authorArray[i]
-
-    let product = {...book.dataValues, genres, authors}
-
-    return product
-  })
-
-  res.json(bookArray)
-})
+  res.json(books)
+});
