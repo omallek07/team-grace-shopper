02a619269759f76782d25ba8b7f6a93eabbae4f4
mikekanter6@gmail.com <mikekanter@Mikes-MacBook-Pro.local>
2018-01-11 10:15:32 -0600

    all books route

--- server/api/books.js
+++ server/api/books.js
@@ -1,23 +1,36 @@
 const router = require('express').Router()
-const {Book, Genre, Author} = require('../db/models')
+const {Book} = require('../db')
 module.exports = router
 
 router.get('/', async (req, res, next) => {
-  const booksPromise = Book.findAll()
+  let booksPromise = Book.findAll()
 
   let books = await booksPromise
-  let genrePromiseArray = []
-  let authorPromiseArray = []
 
-  books.forEach(book => {
-    genrePromiseArray.push(book.getGenres())
-    authorPromiseArray.push(book.getAuthors())
-  })
+  let genrePromiseArray = Promise.all(books.map(book => {
+    return book.getGenres()
+    .then(genres => genres[0] && genres.map(genre => (
+      {id: genre.dataValues.id, name: genre.dataValues.name}
+    )))
+  })).catch(console.error)
 
-  let bookArray = books.map(async (book, i) => {
-    let genres = await genrePromiseArray[i]
-    let authors = await authorPromiseArray[i]
-    return {...book, genres, authors}
+  let authorPromiseArray = Promise.all(books.map(book => {
+    return book.getAuthors()
+    .then(authors => authors[0] && authors.map(author => (
+      {id: author.dataValues.id, firstName: author.dataValues.firstName, lastName: author.dataValues.lastName}
+    )))
+  })).catch(console.error)
+
+  const genreArray = await genrePromiseArray
+  const authorArray = await authorPromiseArray
+
+  let bookArray = books.map((book, i) => {
+    let genres = genreArray[i]
+    let authors = authorArray[i]
+
+    let product = {...book.dataValues, genres, authors}
+
+    return product
   })
 
   res.json(bookArray)
