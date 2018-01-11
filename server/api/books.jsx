01531f9957aae0554d0cc6be6eccd3a97038e1bb
rutvikhp <rutvikhp@gmail.com>
2018-01-11 10:46:35 -0600

    any

---
+++ server/api/books.jsx
@@ -0,0 +1,25 @@
+const router = require('express').Router()
+const {Book, Genre, Author} = require('../db/models')
+module.exports = router
+
+router.get('/', async (req, res, next) => {
+  const booksPromise = Book.findAll()
+
+  let books = await booksPromise
+  let genrePromiseArray = []
+  let authorPromiseArray = []
+//gdbjha
+  books.forEach(book => {
+    genrePromiseArray.push(book.getGenres())
+    authorPromiseArray.push(book.getAuthors())
+  })
+
+  let bookArray = books.map(async (book, i) => {
+    let genres = await genrePromiseArray[i]
+    let authors = await authorPromiseArray[i]
+    return {...book, genres, authors}
+  })
+
+  res.json(bookArray)
+
+})
