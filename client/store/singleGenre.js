cb3afbe97d593647dbe02c7483a84db0115bee91
Yates Walker <yateswalker@gmail.com>
2018-01-13 13:01:39 -0600

    creates allgenres and added to seed file

---
+++ client/store/singleGenre.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+const GET_GENRE_BY_ID = 'GET_GENRE_BY_ID';
+
+
+const getGenreByIdAction = (singleBook) => {
+  return {
+    type: GET_GENRE_BY_ID,
+    singleGenre
+  }
+}
+
+export const getGenreByIdThunk = (id) => dispatch => {
+  return axios
+    .get(`/api/genres/${id}`)
+    .then(res => res.data)
+    .then(singleGenre => dispatch(getGenreByIdAction(singleGenre)))
+    .catch(err => console.log(err));
+  }
+
+export default function(singleGenre = {}, action) {
+    switch (action.type) {
+      case GET_GENRE_BY_ID:
+        return action.singleGenre
+      default:
+        return singleGenre;
+    }
+  }
